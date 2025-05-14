8baa7fcab511af573d428914de42cfc08aa614c9
imjayeshjadhav <14jayeshjadhav@gmail.com>
2025-05-14 20:02:51 +0530

    Models and global error handling

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ import userRouter from './routes/user.routes.js'
 import authRouter from './routes/auth.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
 import connect from './database/mongodb.js'
+import errorMiddleware from './middlewares/error.middleware.js'
+import cookieParser from 'cookie-parser'
 
 const app =express()
 
-// api/v1/auth/sign-up
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser()) // Reads cookies from incoming request for storing user data
+
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/user', userRouter)
 app.use('/api/v1/subscription', subscriptionRouter)
+
+app.use(errorMiddleware)
 
 app.get('/',(req,res)=>{
     res.send("Welcome to the app")
