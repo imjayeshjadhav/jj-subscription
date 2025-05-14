3bdc71383fb6b2ac2547e6b8de3f3c5d8884a0be
imjayeshjadhav <14jayeshjadhav@gmail.com>
2025-05-14 18:13:07 +0530

    Created Express Server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("JJAYESH")
+import express from 'express'
+
+const app =express()
+
+app.get('/',(req,res)=>{
+    res.send("Hello")
+})
+
+app.listen(5000, ()=>{
+    console.log("Server running on port http://localhost:5000")
+})
+
+export default app;
